9d144172352263f1a7b2058a13659d18ed3816d8
IloveNooodles <mgarebaldhie80@gmail.com>
2022-09-14 02:53:00 +0700

    feat: adding comments

--- LocalSearchBot.py
+++ LocalSearchBot.py
@@ -7,37 +7,42 @@ import math
 import numpy as np
 
 class LocalSearchBot(Bot):
-    def __init__(self, initial_temperature: float = 0, schedule: Callable[[int], float] = lambda t: math.e ** (-t), precision: float = 1E-300, is_player1: bool = False) -> None:
-        self.initial_temperature = initial_temperature
-        self.schedule = schedule
-        self.precision = precision
-        self.is_player1 = is_player1
+    # Inisialisasi Variable awal
+    def __init__( initial_temperature: float = 0, schedule: Callable[[int], float] = lambda t: math.e ** (-t), precision: float = 1E-300, is_player1: bool = False) -> None:
+        initial_temperature = initial_temperature
+        schedule = schedule
+        precision = precision
+        is_player1 = is_player1
 
-    def get_action(self, state: GameState) -> GameAction:
-        current = self.get_random_action(state)
+    # Pemilihan aksi yang akan dilakukan agent
+    def get_action( state: GameState) -> GameAction:
+        current = get_random_action(state)
         time = 1
         while True:
-            current_temperature = self.schedule(time)
-            if abs(current_temperature - self.initial_temperature) <= self.precision:
+            # Perhitungan delta dengan presisi 1e-300
+            current_temperature = schedule(time)
+            if abs(current_temperature - initial_temperature) <= precision:
                 break
 
-            next = self.get_random_action(state)
-            delta = self.get_value(state, next) - self.get_value(state, current)
+            next = get_random_action(state)
+            delta = get_value(state, next) - get_value(state, current)
 
-
+            # Jika delta positif atau tolerable maka ambil langkah selanjutnya
             if delta > 0 or random.random() < math.e ** (delta / current_temperature):
                 current = next
             time += 1
 
         return current
 
-    def get_random_action(self, state: GameState) -> GameAction:
-        actions = self.generate_actions(state)
+    # Pemilihan aksi random dari list yang tersedia
+    def get_random_action( state: GameState) -> GameAction:
+        actions = generate_actions(state)
         return random.choice(actions)
 
-    def generate_actions(self, state: GameState) -> List[GameAction]:
-        row_positions = self.generate_positions(state.row_status)
-        col_positions = self.generate_positions(state.col_status)
+    # Generate list aksi yang bisa dilakukan
+    def generate_actions( state: GameState) -> List[GameAction]:
+        row_positions = generate_positions(state.row_status)
+        col_positions = generate_positions(state.col_status)
         actions: List[GameAction] = []
 
         for position in row_positions:
@@ -47,7 +52,8 @@ class LocalSearchBot(Bot):
 
         return actions
 
-    def generate_positions(self, matrix: np.ndarray) -> List[tuple[int, int]]:
+    # Generate posisi dari setiap garis yang masih kosong
+    def generate_positions( matrix: np.ndarray) -> List[tuple[int, int]]:
         [ny, nx] = matrix.shape
         positions: List[tuple[int, int]] = []
 
@@ -58,16 +64,20 @@ class LocalSearchBot(Bot):
 
         return positions
 
-    def get_result(self, state: GameState, action: GameAction) -> GameState:
+    # Update board
+    def get_result( state: GameState, action: GameAction) -> GameState:
         type = action.action_type
         x, y = action.position
 
+        # Dilakukan copy agar tidak mengubah status game
         new_state = GameState(state.board_status.copy(), state.row_status.copy(), state.col_status.copy(), state.player1_turn)
         player_modifier = -1 if new_state.player1_turn else 1
         is_point_scored = False
         val = 1
 
         [ny, nx] = new_state.board_status.shape
+        
+        # Pengecekan apakah akan terbentuk box pada move ini
         if y < ny and x < nx:
             new_state.board_status[y, x] = (abs(new_state.board_status[y, x]) + val) * player_modifier
             if abs(new_state.board_status[y, x]) == 4:
@@ -86,11 +96,14 @@ class LocalSearchBot(Bot):
                 if abs(new_state.board_status[y, x - 1]) == 4:
                     is_point_scored = True
 
+        # Jika bot di player 1 maka kita ingin bot mendapatkan point sebesar mungkin maka langkah ini akan diambil
+        # Jika bot berada di player 2 maka kita ingin player 1 tidak mendapatkan poin sehingga kita ingin mengambil aksi dimana player 1 tidak mendapatkan poin
         new_state = new_state._replace(player1_turn = not (new_state.player1_turn ^ is_point_scored))
         return new_state
 
-    def get_value(self, state: GameState, action: GameAction) -> float:
-        new_state = self.get_result(state, action)
+    # Utility function dengan nilai absolute 1 jika box terbentuk. 
+    def get_value( state: GameState, action: GameAction) -> float:
+        new_state = get_result(state, action)
         [ny, nx] = new_state.board_status.shape
         utility = 0
 
@@ -101,4 +114,4 @@ class LocalSearchBot(Bot):
                 elif new_state.board_status[y, x] == -4:
                     utility -= 1
 
-        return -utility if self.is_player1 else utility
+        return -utility if is_player1 else utility
