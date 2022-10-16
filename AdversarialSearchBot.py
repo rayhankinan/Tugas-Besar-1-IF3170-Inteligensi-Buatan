c4f4d48687897c0450bdf391722020a1abe25ff1
fernaldyl <90769501+fernaldy112@users.noreply.github.com>
2022-10-16 08:00:44 +0700

    perf: implemented chain rule

--- AdversarialSearchBot.py
+++ AdversarialSearchBot.py
@@ -96,6 +96,7 @@ class AdversarialSearchBot(Bot):
         # Udah ketemu solusi tinggal dihitung dengan fungsi objektif
         # Dilakukan pembatasan depth agar tidak terlalu lama
         # TODO: Mengganti depth first search menjadi iterative deepening search (untuk menghilangkan kebutuhan max_depth)
+
         if self.terminal_test(state) or depth == self.max_depth:
             return self.get_utility(state)
 
@@ -153,4 +154,70 @@ class AdversarialSearchBot(Bot):
                         utility -= 1
                     elif state.board_status[y, x] == 4:
                         utility += 1
+
+        if self.chain_count(state) % 2 == 0 and self.is_player1:
+            utility += 3
+        elif self.chain_count(state) % 2 != 0 and not self.is_player1:
+            utility += 3
+            
         return utility
+
+        # Count the number of long chain(s)
+    def chain_count(self, state: GameState) -> int:
+
+        chain_count = 0
+        chain_list : List[List[int]] = []
+
+        for box_num in range(9):
+
+            # check if box is already part of a chain
+            flag = False
+            for chain in chain_list:
+                if box_num in chain:
+                    flag = True
+                    break
+
+            if not flag:
+                chain_list.append([box_num])
+                self.add_chain(state, chain_list, box_num)
+
+        for chain in chain_list:
+            if len(chain) >= 3:
+                chain_count += 1
+
+        return chain_count
+                
+    # Find adjacent box(es) which can build chain
+    def add_chain(self, state: GameState, chain_list: List[List[int]], box_num):
+
+        neighbors_num = [
+            box_num - 1,
+            box_num - 3,
+            box_num + 1,
+            box_num + 3
+        ]
+
+        for idx in range(len(neighbors_num)):
+
+            if neighbors_num[idx] < 0 or \
+                neighbors_num[idx] > 8 or \
+                (idx%2==0 and neighbors_num[idx]//3 != box_num//3):
+                continue
+            
+            flag = False
+            for chain in chain_list:
+                if neighbors_num[idx] in chain:
+                    flag = True
+                    break
+
+            if not flag and idx%2==0:
+                reference = max(box_num, neighbors_num[idx])
+                if not state.col_status[reference // 3][reference % 3]:
+                    chain_list[-1].append(neighbors_num[idx])
+                    self.add_chain(state, chain_list, neighbors_num[idx])
+
+            if not flag and idx%2!=0:
+                reference = max(box_num, neighbors_num[idx])
+                if not state.row_status[reference // 3][reference % 3]:
+                    chain_list[-1].append(neighbors_num[idx])
+                    self.add_chain(state, chain_list, neighbors_num[idx])
--- LocalSearchBot.py
+++ LocalSearchBot.py
@@ -110,6 +110,7 @@ class LocalSearchBot(Bot):
 
     # Utility function dengan nilai absolute 1 jika box terbentuk.
     def get_value(self, state: GameState, action: GameAction) -> float:
+
         new_state = self.get_result(state, action)
         [ny, nx] = new_state.board_status.shape
         utility = 0
@@ -129,4 +130,71 @@ class LocalSearchBot(Bot):
                         utility -= 1
                     elif new_state.board_status[y, x] == 4:
                         utility += 1
+
+        if self.chain_count(new_state) % 2 == 0 and self.is_player1:
+            utility += 3
+        elif self.chain_count(new_state) % 2 != 0 and not self.is_player1:
+            utility += 3
+
         return utility
+
+    # Count the number of long chain(s)
+    def chain_count(self, state: GameState) -> int:
+
+        chain_count = 0
+        chain_list : List[List[int]] = []
+
+        for box_num in range(9):
+
+            # check if box is already part of a chain
+            flag = False
+            for chain in chain_list:
+                if box_num in chain:
+                    flag = True
+                    break
+
+            if not flag:
+                chain_list.append([box_num])
+                self.add_chain(state, chain_list, box_num)
+
+        for chain in chain_list:
+            if len(chain) >= 3:
+                chain_count += 1
+
+        return chain_count
+                
+    # Find adjacent box(es) which can build chain
+    def add_chain(self, state: GameState, chain_list: List[List[int]], box_num):
+
+        neighbors_num = [
+            box_num - 1,
+            box_num - 3,
+            box_num + 1,
+            box_num + 3
+        ]
+
+        for idx in range(len(neighbors_num)):
+
+            if neighbors_num[idx] < 0 or \
+                neighbors_num[idx] > 8 or \
+                (idx%2==0 and neighbors_num[idx]//3 != box_num//3):
+                continue
+            
+            flag = False
+            for chain in chain_list:
+                if neighbors_num[idx] in chain:
+                    flag = True
+                    break
+
+            if not flag and idx%2==0:
+                reference = max(box_num, neighbors_num[idx])
+                if not state.col_status[reference // 3][reference % 3]:
+                    chain_list[-1].append(neighbors_num[idx])
+                    self.add_chain(state, chain_list, neighbors_num[idx])
+
+            if not flag and idx%2!=0:
+                reference = max(box_num, neighbors_num[idx])
+                if not state.row_status[reference // 3][reference % 3]:
+                    chain_list[-1].append(neighbors_num[idx])
+                    self.add_chain(state, chain_list, neighbors_num[idx])
+
